ecfbfa61f43de27ad7cbbdd8559f66883cf2f469
baggio-snir <Quentin.Chassel@ac-lille.fr>
2022-07-03 14:44:23 +0200

    Bases du scrollspy qui fonctionne désormais

--- assets/js/magic-scroll-spy.js
+++ assets/js/magic-scroll-spy.js
@@ -1,7 +1,39 @@
 
-let sections = ['presentation', 'apprentissage', 'temoignages'];
-let currentSection = 0;
-
-window.onscroll = function (ev) {
-    
+let sections = {
+    'presentation': 'smart-aside-coords',
+    'baggio': 'smart-aside-coords',
+    'techs': 'smart-aside-techs',
+    'plateaux': 'smart-aside-summary',
+    'apprentissage': 'smart-aside-ufa',
+    'temoignages': 'smart-aside-stats',
 };
+
+let options = {
+    root: document.querySelector(null),
+    rootMargin: '0px',
+    threshold: 1.0
+}
+
+let observer = new IntersectionObserver((r) => {
+    const currentTarget = r[0].target.id;
+    let linkedAside = document.querySelector('#'+sections[currentTarget]);
+    if(linkedAside) {
+        let isLinkedAsideVisible = ('none' !== window.getComputedStyle(linkedAside).display);
+        if(r[0].isIntersecting) {
+            if(!isLinkedAsideVisible) {
+                console.info('showing '+sections[currentTarget]);
+                linkedAside.classList.replace('d-none', 'd-block');
+            }
+        } else {
+            if(isLinkedAsideVisible) {
+                console.info('hiding '+sections[currentTarget]);
+                linkedAside.classList.replace('d-block', 'd-none');
+            }
+        }
+    } else {
+        console.error('Unable to find #'+sections[currentTarget]+' : no linked aside content will be shown');
+    }
+}, options);
+for(const x in sections) {
+    observer.observe(document.querySelector('#'+x));
+}
